7cea615f273addbe8013194462be7d243e06a698
Phantasia0 <ncprog1@gmail.com>
2023-10-24 13:17:02 +0900

    uncontrolled-flow-update

--- controlled_uncontrolled/src/App.tsx
+++ controlled_uncontrolled/src/App.tsx
@@ -1,28 +1,58 @@
 import { UncontrolledFlow } from "./components/uncontorlled-flow";
 
-type NextFn = () => void;
+export interface ParamType {
+  name?: string;
+  age?: number;
+  country?: string;
+}
+
+type NextFn = (param: ParamType) => void;
 
 const StepOne = ({ goNext }: { goNext?: NextFn }) => {
   return (
     <>
-      <h1>Step #1</h1>
-      <button onClick={goNext}>Next</button>
+      <h1>Step #1: Enter your name:</h1>
+      <button
+        onClick={() => {
+          if (goNext) {
+            goNext({ name: "MyName" });
+          }
+        }}
+      >
+        Next
+      </button>
     </>
   );
 };
 const StepTwo = ({ goNext }: { goNext?: NextFn }) => {
   return (
     <>
-      <h1>Step #2</h1>
-      <button onClick={goNext}>Next</button>
+      <h1>Step #2: Enter your age</h1>
+      <button
+        onClick={() => {
+          if (goNext) {
+            goNext({ age: 20 });
+          }
+        }}
+      >
+        Next
+      </button>
     </>
   );
 };
 const StepThree = ({ goNext }: { goNext?: NextFn }) => {
   return (
     <>
-      <h1>Step #3</h1>
-      <button onClick={goNext}>Next</button>
+      <h1>Step #3: Enter your country</h1>
+      <button
+        onClick={() => {
+          if (goNext) {
+            goNext({ country: "Mars" });
+          }
+        }}
+      >
+        Next
+      </button>
     </>
   );
 };
@@ -30,7 +60,12 @@ const StepThree = ({ goNext }: { goNext?: NextFn }) => {
 function App() {
   return (
     <>
-      <UncontrolledFlow onDone={false}>
+      <UncontrolledFlow
+        onDone={(data) => {
+          console.log(data);
+          alert("Final Step Completed!");
+        }}
+      >
         <StepOne />
         <StepTwo />
         <StepThree />
--- controlled_uncontrolled/src/components/uncontorlled-flow.tsx
+++ controlled_uncontrolled/src/components/uncontorlled-flow.tsx
@@ -1,16 +1,32 @@
 import React, { useState } from "react";
+import { ParamType } from "../App";
 
 interface FlowProps {
-  children: React.ReactNode;
-  onDone: boolean;
+  children: React.ReactNode[];
+  onDone: (param: ParamType) => void;
 }
 
 export const UncontrolledFlow: React.FC<FlowProps> = ({ children, onDone }) => {
   const [data, setData] = useState<Object>({});
   const [currentStepIndex, setCurrentStepIndex] = useState<number>(0);
 
-  const goNext = () => {
-    setCurrentStepIndex((prevState) => prevState + 1);
+  const goNext = (dataFromStep: ParamType) => {
+    const nextStepIndex = currentStepIndex + 1;
+
+    const newData = {
+      ...data,
+      ...dataFromStep,
+    };
+
+    console.log(newData);
+
+    if (nextStepIndex < children.length) {
+      setCurrentStepIndex((prevState) => prevState + 1);
+    } else {
+      onDone(newData);
+    }
+
+    setData(newData);
   };
 
   const currentChild = React.Children.toArray(children)[currentStepIndex];
